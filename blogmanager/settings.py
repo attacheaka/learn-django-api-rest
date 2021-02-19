a82c5b67ebdddea83beab56e64b344c7214154ed
Attache <attacheaka66@gmail.com>
2021-02-19 16:46:24 +0000

    Refactoring , static , settings , requirements

--- blogmanager/settings.py
+++ blogmanager/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+# others static file STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
--- blogmanager/urls.py
+++ blogmanager/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('blog.urls')),
     path('', include('accounts.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
