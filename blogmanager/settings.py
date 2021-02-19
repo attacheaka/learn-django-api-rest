8232789e2b6c8268f3304b32837e111f82dfee4c
Attache <attacheaka66@gmail.com>
2021-02-19 17:16:25 +0000

    Add staticfiles

--- blogmanager/settings.py
+++ blogmanager/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
